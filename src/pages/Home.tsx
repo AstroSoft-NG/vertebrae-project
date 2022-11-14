e04b89168a96f3db27497a2a382f8432443ed037
Agent Ebimene <ebimeneagent@gmail.com>
2022-11-14 14:11:21 +0100

    Add up Home page to app and react router to app

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import whiteLogo from '../assets/images/white-logo.png';
+
+const Home = () => {
+  return (
+    <section className='hero-section'>
+      <Link className='hero-link' to='/products' target='_blank' rel='noopener noreferrer'>
+        <img src={whiteLogo} alt='Hero Logo' />
+      </Link>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/pages/Products.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+  return (
+    <div>
+      <h2>Here are the products</h2>
+    </div>
+  );
+};
+
+export default Products;
